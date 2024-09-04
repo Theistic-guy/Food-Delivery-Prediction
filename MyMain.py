f2acb964984610c4e2e1dec176dc1467cea30411
Theistic-guy <aryamanverma11@gmail.com>
2024-09-04 23:47:38 +0530

    new commits

--- MyMain.py
+++ MyMain.py
@@ -1,1 +1,77 @@
+import pickle
+import pandas as pd
 import streamlit as st
+import numpy as np
+from geopy.distance import geodesic
+from sklearn.preprocessing import LabelEncoder
+import requests
+
+
+with open('scaler.pkl', 'rb') as f:
+    scaler = pickle.load(f)
+
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+
+
+def extract_column_value(df):
+    # #Extract time and convert to int
+    # df['Time_taken(min)'] = df['Time_taken(min)'].apply(lambda x: int(x.split(' ')[1].strip()))
+    #Extract Weather conditions
+    # df['Weather_conditions'] = df['Weather_conditions'].apply(lambda x: x.split(' ')[1].strip())
+    #Extract city code from Delivery person ID
+    df['City_code']=df['Delivery_person_ID'].str.split("RES", expand=True)[0]
+    
+
+
+
+
+def extract_date_features(data):
+    data["day"] = data.Order_Date.dt.day
+    data["month"] = data.Order_Date.dt.month
+    data["quarter"] = data.Order_Date.dt.quarter
+    data["year"] = data.Order_Date.dt.year
+    data['day_of_week'] = data.Order_Date.dt.day_of_week.astype(int)
+    data["is_month_start"] = data.Order_Date.dt.is_month_start.astype(int)
+    data["is_month_end"] = data.Order_Date.dt.is_month_end.astype(int)
+    data["is_quarter_start"] = data.Order_Date.dt.is_quarter_start.astype(int)
+    data["is_quarter_end"] = data.Order_Date.dt.is_quarter_end.astype(int)
+    data["is_year_start"] = data.Order_Date.dt.is_year_start.astype(int)
+    data["is_year_end"] = data.Order_Date.dt.is_year_end.astype(int)
+    data['is_weekend'] = np.where(data['day_of_week'].isin([5,6]),1,0)
+
+
+def calculate_time_diff(df):
+    # Find the difference between ordered time & picked time
+    df['Time_Orderd'] = pd.to_timedelta(df['Time_Orderd'])
+    
+    df['Time_Order_picked'] = pd.to_timedelta(df['Time_Order_picked'])
+    
+    df['Time_Order_picked_formatted'] = df['Order_Date'] + np.where(df['Time_Order_picked'] < df['Time_Orderd'], pd.DateOffset(days=1), pd.DateOffset(days=0)) +     df['Time_Order_picked']
+    df['Time_Ordered_formatted'] = df['Order_Date'] + df['Time_Orderd']
+
+    df['Time_Order_picked_formatted']=pd.to_datetime(df['Time_Order_picked_formatted'])
+    
+    df['order_prepare_time'] = (df['Time_Order_picked_formatted'] - df['Time_Ordered_formatted']).dt.total_seconds() / 60
+    
+    # Handle null values by filling with the median
+    df['order_prepare_time'].fillna(df['order_prepare_time'].median(), inplace=True)
+    
+    # Drop all the time & date related columns
+    df.drop(['Time_Orderd', 'Time_Order_picked', 'Time_Ordered_formatted', 'Time_Order_picked_formatted', 'Order_Date'], axis=1, inplace=True)
+
+
+
+#Calculate distance between restaurant location & delivery location
+def calculate_distance(df):
+    df['distance']=np.zeros(len(df))
+    restaurant_coordinates=df[['Restaurant_latitude','Restaurant_longitude']].to_numpy()
+    delivery_location_coordinates=df[['Delivery_location_latitude','Delivery_location_longitude']].to_numpy()
+    df['distance'] = np.array([geodesic(restaurant, delivery) for restaurant, delivery in zip(restaurant_coordinates, delivery_location_coordinates)])
+    df['distance']= df['distance'].astype("str").str.extract('(\d+)').astype("int64")
+    
+def label_encoding(df):
+    categorical_columns = df.select_dtypes(include='object').columns
+    label_encoder = LabelEncoder()
+    df[categorical_columns] = df[categorical_columns].apply(lambda col: label_encoder.fit_transform(col))
